8604af3f9cb444d61ed3d69ade10867705acfd44
Christian Mikovits <christian.mikovits@boku.ac.at>
2025-02-07 16:20:47 +0100

    added area in dict

--- windlulc/windlulc.py
+++ windlulc/windlulc.py
@@ -308,7 +308,7 @@ def main(yaml_filename: str, rasterdata: dict, geojson_dict: dict, DEBUG=False):
     turbines_gdf["buffer"] = cluster_distance(turbines_gdf["geometry"])
     # create a hull around the turbines (singlebuffer, concave or convex) also using the average distance
     hull_gdf = create_hulls(turbines_gdf, hull_type=config["hulltype"])
-    print(hull_gdf)
+    # begin the result dictionary with prefilled area
     result = {"area_ha": int(hull_gdf["geometry"][0].area/10000)}
 
     for name, values in rasterdata.items():
